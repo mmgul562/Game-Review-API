7a3e9ac5480dc0df6ce501cc3384975c6ba4f855
mmgul562 <mikolaj.gulczas@gmail.com>
2024-06-21 11:35:55 +0200

    Adjust Django-Admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+
+class UserAdmin(DjangoUserAdmin):
+    ordering = ['id', 'username']
+    list_display = ['username', 'email']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'name', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (_('Additional Information'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {'fields': (
+            'username',
+            'email',
+            'password1',
+            'password2',
+            'name',
+            'is_active',
+            'is_staff',
+            'is_superuser'
+        ), }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/tests.py
+++ app/core/tests.py
@@ -1,39 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
 
-class ModelTests(TestCase):
-    @staticmethod
-    def create_user(email="test@example.com",
-                    username="example",
-                    passwd="example123"):
-        return get_user_model().objects.create_user(email, username, passwd)
+def create_user(email="test@example.com",
+                username="example",
+                passwd="example123"):
+    return get_user_model().objects.create_user(email, username, passwd)
 
-    def test_create_user_successful(self):
-        user = ModelTests.create_user()
+
+class ModelTests(TestCase):
+    def test_create_user(self):
+        user = create_user()
         self.assertEqual(user.email, "test@example.com")
         self.assertEqual(user.username, "example")
         self.assertNotEqual(user.password, "example123")
         self.assertTrue(user.check_password("example123"))
 
     def test_create_user_with_duplicate_email(self):
-        ModelTests.create_user("duplicate@example.com", "example")
+        create_user("duplicate@example.com", "example")
         with self.assertRaises(IntegrityError):
-            ModelTests.create_user("duplicate@example.com", "different")
+            create_user("duplicate@example.com", "different")
 
     def test_create_user_with_duplicate_username(self):
-        ModelTests.create_user("test@example.com", "duplicate")
+        create_user("test@example.com", "duplicate")
         with self.assertRaises(IntegrityError):
-            ModelTests.create_user("different@example.com", "duplicate")
+            create_user("different@example.com", "duplicate")
 
     def test_create_user_without_needed_credentials(self):
         with self.assertRaises(ValueError):
-            ModelTests.create_user("")
+            create_user("")
         with self.assertRaises(ValueError):
-            ModelTests.create_user("test@example.com", "")
+            create_user("test@example.com", "")
 
-    def test_create_superuser_successful(self):
+    def test_create_superuser(self):
         user = get_user_model().objects.create_superuser(
             "test@example.com",
             "superuser",
@@ -41,3 +42,32 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class AdminTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            username="admin",
+            password="example123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = create_user()
+
+    def test_get_users_list(self):
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.username)
+        self.assertContains(res, self.user.email)
+
+    def test_get_user_edit_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_user_add_page(self):
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
